9994381f9105ff671eba13a2f752bec9cfb9b00e
alextaylor <alextaylor515@gmail.com>
2022-07-18 16:34:09 -0400

    commit tree b1602ba0f662814ba005e3d1a8d350918a55ced7

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
 # your code goes here
+
+class Person
+    attr_reader :name 
+    attr_accessor :bank_account
+    attr_accessor :hygiene
+    def initialize name, bank_account=25, happiness=8, hygiene=8
+        @name = name
+        @bank_account = bank_account
+        @hygiene = hygiene 
+        @happiness = happiness
+    end
+    def happiness 
+        if self.happiness < 10 && self.happiness > 0
+            @happiness = self.happiness
+        end
+    end
+    def happiness
+        @happiness
+    end
+
+end
